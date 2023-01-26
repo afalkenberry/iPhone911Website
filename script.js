3fdebb9fad9105dcaa74c4d5a7aa098b88715c5b
Alec Falkenberry <101435646+afalkenberry@users.noreply.github.com>
2023-01-26 12:56:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,69 @@
-const accordionItems = document.querySelectorAll('.collapse')
-const acc = document.getElementById('accordionExample')
-console.log(accordionItems);
+$(document).ready(function(){
+    var navbar = document.getElementById("navbar");
+var navbarHeight = navbar.getBoundingClientRect().height;
+  //iphone dropdown in buy section
+    $("#iphonedropdown img").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#accordion").offset().top - navbarHeight
+        }, 1000);
+        $("#iphonecollapse").slideToggle();
+    });
+  //samsung dropdown in buy section
+   $("#samsungdropdown img").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#accordion").offset().top - navbarHeight
+        }, 1000);
+        $("#samsungcollapse").slideToggle();
+    });
+   
+  //repair iphone
+  $("#repairiphone img").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#accordion").offset().top - navbarHeight
+        }, 1000);
+        $("#iphonecard").slideToggle();
+    });
+  //repair ipad
+  $("#repairipad img").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#accordion").offset().top - navbarHeight
+        }, 1000);
+        $("#ipadcard").slideToggle();
+    });
+  //repair android
+  $("#repairandroid img").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#accordion").offset().top - navbarHeight
+        }, 1000);
+        $("#samsungcard").slideToggle();
+    });
+  //repair mac
+  $("#repairmac img").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#accordion").offset().top - navbarHeight
+        }, 1000);
+        $("#maccard").slideToggle();
+    });
+  //repair microsoldering
+  $("#repairmicrosoldering img").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#accordion").offset().top - navbarHeight
+        }, 1000);
+        $("#microsolderingcard").slideToggle();
+    });
+  //repair srs
+  $("#repairsrs img").click(function(){
+        $('html, body').animate({
+            scrollTop: $("#accordion").offset().top - navbarHeight
+        }, 1000);
+        $("#srscard").slideToggle();
+    });
+}); 
 
-accordionItems.forEach((el)=>{
-    el.addEventListener('shown.bs.collapse',(e)=>{
-        var scrollOffset = acc.scrollTop + el.parentNode.offsetTop
-        acc.scroll({
-            top: scrollOffset,
-            left: 0, 
-            behavior: 'smooth'
-        })
-    })
-})
+/*back button and automatically collapse accordion and scroll to top of the page*/
+
+document.getElementById("back-button").addEventListener("click", function(){
+    var accordion = document.getElementById("iphonecollapse");
+    var collapse = accordion.getElementsByClassName("collapse");
+    collapse[0].classList.remove("show");
+});
